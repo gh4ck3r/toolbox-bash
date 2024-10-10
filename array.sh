d3cbab25f2c975b8b0b205dd01428f4de01af67f
Changbin Park <gh4ck3r@gmail.com>
2024-10-10 21:59:16 +0900

    add arr-{dump,sort}

---
+++ array.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+function arr-dump
+{
+  : ${1?name of array}
+  declare -n var=$1
+  local k
+  for k in ${!var[@]}; do
+    echo -e "$1[$k]: ${var[$k]}"
+  done
+}
+
+function arr-sort
+{
+  : ${1?name of array}
+  [[ $(declare -p $1 2>/dev/null) = "declare -a"* ]] || return 22
+  declare -n var=$1
+
+  local _IFS=$IFS
+  IFS=$'\n' var=($(sort -n <<<${var[*]}))
+  IFS=_IFS
+}
+
+# "sourced script" ends here
+return 0 2>/dev/null
+
+declare unsorted_arr=(4 2 3 1)
+echo "===================="
+echo "arr-dump unsorted_arr"
+echo --------------------
+arr-dump unsorted_arr
+echo "===================="
+echo arr-sort unsorted_arr
+arr-sort unsorted_arr
+echo "===================="
+echo "arr-dump unsorted_arr"
+echo --------------------
+arr-dump unsorted_arr
+echo ====================
